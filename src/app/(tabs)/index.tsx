cf7482f9681a92c1c089fdae6f7d9530fa1c8879
Rokas <rookas.rudzenskas@gmail.com>
2023-10-07 09:48:00 +0300

    flight option

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,6 +1,8 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {FlatList, StyleSheet, Text, View} from 'react-native';
 import SearchForm from "../../components/SearchForm";
 import {LinearGradient} from "expo-linear-gradient";
+import items from "../../../assets/data.json";
+import FlightOptionItem from "../../components/FlightOptionItem";
 
 export default function TabOneScreen() {
   const onSearch = async (data: any) => {
@@ -9,8 +11,16 @@ export default function TabOneScreen() {
 
   return (
     <LinearGradient colors={['white', '#f1f6fc']} className="flex flex-1 pt-16">
-      <SearchForm
-        onSearch={onSearch}
+      <View>
+        <SearchForm
+          onSearch={onSearch}
+        />
+      </View>
+
+      <FlatList
+        data={items}
+        renderItem={({ item }) => <FlightOptionItem flight={item} />}
+        showsVerticalScrollIndicator={false}
       />
     </LinearGradient>
   );
