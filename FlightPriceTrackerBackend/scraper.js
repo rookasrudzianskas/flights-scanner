f5a3c11a4dfe4e3d30f622415c948262559be15f
Rokas <rookas.rudzenskas@gmail.com>
2023-10-07 09:59:30 +0300

    started dev of bright data

---
+++ FlightPriceTrackerBackend/scraper.js
@@ -0,0 +1,5 @@
+const puppeteer = require('puppeteer-core');
+const { openDevtools } = require('./utils');
+
+const SBR_WS_ENDPOINT =
+    'wss://brd-customer-hl_fab45111-zone-flight_prices:05g2io2ojt2z@brd.superproxy.io:9222';
